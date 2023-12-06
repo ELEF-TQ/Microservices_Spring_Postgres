bc1fbd68db4ae93621371db1a980243b4dcaa97c
ELEF-TQ <tariq75aitlakhlift@gmail.com>
2023-12-06 15:55:02 +0100

    api gateway

--- cloud-gateway/src/main/java/com/dailycodebuffer/cloudgateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/dailycodebuffer/cloudgateway/CloudGatewayApplication.java
@@ -2,8 +2,11 @@ package com.dailycodebuffer.cloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
